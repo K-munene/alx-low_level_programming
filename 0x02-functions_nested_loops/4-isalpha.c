d94cb49b4030c19bf64b58e09515d59b819eab3b
Edwin Karani <muneneedwin23@gmail.com>
2023-07-01 03:52:56 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,7 +13,7 @@ int _isalpha(int c)
 	char lower, upper;
 	int letter = 0;
 
-	for (lower = 'a'; lower <= 'z'; lower++;)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
