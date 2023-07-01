1e9a297c0f708a7417c9935f22fb34f65528a742
Edwin Karani <muneneedwin23@gmail.com>
2023-07-01 01:40:02 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_*10 -  prints alphabets *10
+ *
+ * Description: using _putchar to print the alphabets
+ *
+ */
+void void print_alphabet_x10(void);
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'' i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
+
