1090c5ee6548c8309a56593f4e886c8f9dd0b3b3
edwin.munene <muneneedwin23@gmail.com>
2023-09-19 06:56:38 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
  *@s: input
  *Return: Always 0 (Success)
 */
-void_puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursions(s + 1);
 	}
+
 	else
 		_putchar("\n");
 }
