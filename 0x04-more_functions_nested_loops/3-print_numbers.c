bd33fc7be2e3661f4ef677ce5a30990affba010b
K-munene <muneneedwin23@gmail.com>
2024-02-16 18:44:47 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - func checks for a digit (0 to 9)
- *
- * Return : Always 0
+ * print_numbers - function which checks for a digit (0 to 9)
+ * Return: Always 0
  */
 void print_numbers(void);
 {
@@ -15,3 +14,7 @@ void print_numbers(void);
 	}
 		_putchar('\n');
 }
+
+
+
+
