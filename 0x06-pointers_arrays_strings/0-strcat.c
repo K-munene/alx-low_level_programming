e5c56752dafbb60ee38638bbe1b64ebda3c9607c
K-munene <muneneedwin23@gmail.com>
2024-02-16 17:25:14 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len1, len2, i;
+	int i;
+	int j;
 
-	for (len1 = 0; dest[len1] != '\0'; len1++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-
+		i++;
 	}
-
-	for (len2 = 0; src[len2] != '\0'; len2++)
+	j = 0;
+	while (src[j] != '\0')
 	{
-
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	for (i = 0; i < len2; i++)
-	{
-		dest[len1 + i] = src[i];
-	}
-
+	dest[i] = '\0';
 	return (dest);
 }
