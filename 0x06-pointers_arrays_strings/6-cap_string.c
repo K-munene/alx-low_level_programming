17b863ae4c245c71958a964758a4d30d8dc7dc4d
K-munene <muneneedwin23@gmail.com>
2024-03-05 03:00:58 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * cap_string - to capitalize all words of a string
+ * @str: the string to be capitalized
+ * @Return: a pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char c[] = 
