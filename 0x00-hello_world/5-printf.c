3d1bca6e25ac5d10a3a499b344d1c10eeb0f8dd7
K-munene <muneneedwin23@gmail.com>
2024-03-05 05:24:06 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,7 @@
  * main - a main program that prints a line using the printf function
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
