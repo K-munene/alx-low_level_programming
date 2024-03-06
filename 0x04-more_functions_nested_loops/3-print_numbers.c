8346e13445215fdd05142c2e21df520bec774b95
K-munene <muneneedwin23@gmail.com>
2024-03-06 04:57:56 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,15 +4,15 @@
 /**
  * print_numbers - a function which checks for a digit(0 through to 9)
  *
- * Return: Always 0
+ * Return: no return
  */
-void print_numbers(void);
+void print_numbers(void)
 {
-	int c;
+	int d;
 
-	for (c = 48; c < 58; c++)
+	for (d = 48; d <= 57; d++)
 	{
-		_putchar(c);
+		_putchar(d);
 	}
 		_putchar('\n');
 }
