25024e3662a7bfc8fda58131cae326d5af2c6f8f
K-munene <muneneedwin23@gmail.com>
2024-03-06 05:24:06 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
-	for (i = 0; n > 1; i++)
+	if (n <= 0)
 	{
-		if (n > 1)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
+			for (j = 0; j < n; j++)
+		{
+			if (j == i)
+			_putchar('\\');
+			else if (j < i)
+			_putchar(' ');
 		}
-		_putchar('\\');
 		_putchar('\n');
 	}
-
+	}
 }
