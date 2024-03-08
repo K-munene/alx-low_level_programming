19134213befe0f7920c1f68876e0d7424d2df6d5
K-munene <muneneedwin23@gmail.com>
2024-03-08 00:54:25 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,20 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int i, j;
+	
+	for (i = 0; i < n; i++)
 	{
+		if (n > 1)
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
 		_putchar('\n');
-	} else
-	{
-		int i, j;
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-		{
-			if (j == i)
-			_putchar('\\');
-			else if (j < i)
-			_putchar(' ');
-		}
-		_putchar('\n');
 	}
-	}
+
+	if (n <= 0)
+	
 }
