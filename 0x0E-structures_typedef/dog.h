c2c06524663485b978e974a6a9dfeb641d88d443
K-munene <muneneedwin23@gmail.com>
2024-04-25 09:32:08 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct that is to crate info of a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Descript: the first struct
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+
+};
+#endif
