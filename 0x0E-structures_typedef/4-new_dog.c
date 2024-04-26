a707e55f6dbb3d5e44b4d73838955c020561d385
K-munene <muneneedwin23@gmail.com>
2024-04-26 03:43:43 -0700

    repair

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,31 +17,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	{
-		free(dog);
 		return (NULL);
-	}
 	for (n1 = 0; name[n1] != '\0'; n1++)
 		;
 	n1++;
 
-	dog->name = malloc(sizeof(char) * n1);
+	dog->name = malloc(n1 * sizeof(char));
 	if (dog->name == NULL)
 	{
-		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < n1; i++)
 		dog->name[i] = name[i];
 	dog->age = age;
-	for (o1 = 0; owner[o] != '\0'; o1++)
+	for (o1 = 0; owner[o1]; o1++)
 		;
 	o1++;
-	dog->owner = malloc(sizeof(char) * o1);
+	dog->owner = malloc(o1 * sizeof(char));
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
