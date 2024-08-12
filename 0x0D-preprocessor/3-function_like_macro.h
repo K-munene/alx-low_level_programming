30762b25ca11cfd21d3db5b8eeaadecb5dcf8d07
Edwin Munene <muneneedwin23@gmail.com>
2024-08-12 17:47:12 +0300

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * File: 3-function_like_macro.h
+ *
+ * @x: input value (an int)
+ *
+ * Description: header file which compute absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
