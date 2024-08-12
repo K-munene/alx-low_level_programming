2990a89175378bb01d3f50ff4907bddd417205a4
Edwin Munene <muneneedwin23@gmail.com>
2024-08-12 17:24:06 +0300

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,21 +1,10 @@
 #ifndef MACRO_H
 #define MACRO_H
 /**
-<<<<<<< HEAD
- * File : 0-object_like_macro
+ * FILE: 0-object_like_macro.h
  *
- * Description:the header file
+ * Description: a header file
  */
 #define SIZE 1024
 
 #endif
-=======
- * File: 0-object_like_macro.h
- *
- * Description: header file
- *
- */
-#define SIZE 1024
-
-#endif 
->>>>>>> b5f6f6a34bb6e5883d1f05869a7d8ca835d009d1
