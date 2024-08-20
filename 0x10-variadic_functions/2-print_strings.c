6b27a99009c258a05cbd6711ab96938cbc518eb3
Edwin Munene <muneneedwin23@gmail.com>
2024-08-20 15:45:56 +0300

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+/*
+ * print_strings - prints strings pased to a function
+ * as a parameter
+ * @separator: separator between two strings
+ * @n: the number of strings to print
+ *
+ */
+
+{
+	unsigned int i;
+	char *str;
+
+	va_list ptr_str;
+
+	va_start(ptr_str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ptr_str, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	print ("\n");
+	va_end(ptr_str);
+}
